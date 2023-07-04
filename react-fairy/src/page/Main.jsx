c8f6c535f4b99264f961deb134318f08f95db021
ssumniii <jsm5187@gmail.com>
2023-07-04 18:40:35 +0900

    박충희 img upload

--- react-fairy/src/page/Main.jsx
+++ react-fairy/src/page/Main.jsx
@@ -3,11 +3,21 @@ import React from 'react'
 const Main = () => {
   return (
     <div className='Main'>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
+      <section className='Main-sec01'>
+          <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+          </ul>
+      </section>
+      <section className='Main-sec02'>
+          <h2>오늘의 물품상품</h2>
+          <ul>
+              <li><a href=""><h2>tit</h2></a></li>
+              
+             
+          </ul>
+      </section>
     </div>
   )
 }
