8bd6b3430409cc74a9493a2f42ae8ad0f1fcfa46
ssumniii <jsm5187@gmail.com>
2023-07-05 12:20:35 +0900

    조수민-회원가입,로그인

--- react-fairy/src/page/Login.jsx
+++ react-fairy/src/page/Login.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import  {useNavigate} from 'react-router-dom'
+
 
 const Login = () => {
+
+  const idRef = useRef();
+  const pwRef = useRef();
+  
+  const [userData,setUserData] = useState({})
+
+  const handleLogin = (e)=>{
+    e.preventDefault();
+    console.log(idRef.current.value,pwRef.current.value);
+    setUserData({
+      id : idRef.current.value,
+      pw : pwRef.current.value
+    })
+  }
+  useEffect(()=>{
+    userData.id !==undefined &&
+    axios.post('http://localhost:8888/user/login',{userData : userData})
+    .then((res)=>{
+      console.log(res.data.result);
+      if(res.data.result == 'success'){
+        alert(res.data.id+'환영합니다')
+
+        // 브라우저 세션 저장소에 데이터가 저장
+        // => 브라우저를 껏다 키면 사라지는 반휘발성 데이터
+        sessionStorage.setItem('id',res.data.id)
+
+        // 나중을 위해! 세션에 있는 데이터 가져오기!
+        sessionStorage.getItem('id')
+
+      }
+    })
+  },[userData])
+
+
+
+
   return (
-    <div>Login</div>
+    <div className='main-box info-box'>
+    <Form onSubmit={handleLogin}>
+
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>ID</Form.Label>
+          <Form.Control type="text" placeholder="Enter Id" ref={idRef} />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" ref={pwRef} />
+        </Form.Group>
+
+
+
+
+    
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
   )
 }
 
--- react-fairy/src/page/SignUp.jsx
+++ react-fairy/src/page/SignUp.jsx
@@ -1,11 +1,106 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useRef,useState } from 'react';
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
+
+function Join() {
+
+  const nav = useNavigate()
+  
+  const idRef =useRef()
+  const pwRef =useRef()
+  const addRef=useRef()
+
+  const [userData,setUserData]=useState({})
+
+  // ... 코드 아니고 함수 접은거임
+  const handleJoin =(e)=>{
+    console.log('handle Join Function',idRef.current.value,pwRef.current.value,addRef.current.value);
+        
+    // form이 submit 되지 못하도록 작업
+    e.preventDefault();
 
 
-import React from 'react'
+    setUserData({
+      id : idRef.current.value, 
+      pw : pwRef.current.value, 
+      add : addRef.current.value
 
-const SignUp = () => {
+    })
+  }
+
+  useEffect(()=>{
+    console.log('userData : ',userData.id)
+    /*useEffect의 특성 상, 무조건 화면의 첫 갱신 때 함수가 호출될 수 밖에 없다.
+    비어있는 값을 가지고 회원가입을 하면 안되니까 
+    화면의 첫 갱신때는 회원가입 로직이 
+    실행되지 않도록 조건을 걸어둔 것!*/
+    userData.id !==undefined && 
+    axios.post('http://localhost:8888/user/join',{
+      userData : userData
+    })
+    .then((res)=>{
+      console.log('Success',res.data.result);
+      if (res.data.result == 'success'){
+        alert('회원가입을 축하드립니다')
+        nav('/')
+      }else if(res.data.result == 'duplicated'){
+        alert('중복입니다 다시입력하세요')
+        idRef.current.value=''
+        pwRef.current.value=''
+        addRef.current.value=''
+        idRef.current.focus()
+      }
+
+    })
+    .catch(()=>{
+      console.error('실패!')
+    })
+
+  },[userData])
+
+
+
+
+
+
+
   return (
-    <div>SignUp</div>
-  )
+    <div className='main-box info-box'>
+    <Form onSubmit={handleJoin}>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>ID</Form.Label>
+          <Form.Control type="text" placeholder="Enter id" ref={idRef}/>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" ref={pwRef}/>
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" ref={addRef}/>
+      </Form.Group>
+
+    
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
+  );
 }
 
-export default SignUp
+export default Join;
