839a09729fb6ffeb50123234db439e189458bfa9
Holy <ktk0646@gmail.com>
2023-07-05 15:35:08 +0900

    signup

--- react-fairy/src/page/SignUp.jsx
+++ react-fairy/src/page/SignUp.jsx
@@ -7,19 +7,28 @@ import { useEffect, useRef,useState } from 'react';
 import axios from 'axios'
 import {useNavigate} from 'react-router-dom'
 
+
+
 function Join() {
 
   const nav = useNavigate()
   
   const idRef =useRef()
   const pwRef =useRef()
+  const nameRef=useRef()
+  const nickRef=useRef()
   const addRef=useRef()
 
   const [userData,setUserData]=useState({})
 
   // ... 코드 아니고 함수 접은거임
   const handleJoin =(e)=>{
-    console.log('handle Join Function',idRef.current.value,pwRef.current.value,addRef.current.value);
+    console.log('handle Join Function'
+    ,idRef.current.value
+    ,pwRef.current.value
+    ,nameRef.current.value
+    ,nickRef.current.value
+    ,addRef.current.value);
         
     // form이 submit 되지 못하도록 작업
     e.preventDefault();
@@ -28,8 +37,9 @@ function Join() {
     setUserData({
       id : idRef.current.value, 
       pw : pwRef.current.value, 
-      add : addRef.current.value
-
+      name : nameRef.current.value,
+      nick : nickRef.current.value,
+      add : addRef.current.value,      
     })
   }
 
@@ -39,12 +49,12 @@ function Join() {
     비어있는 값을 가지고 회원가입을 하면 안되니까 
     화면의 첫 갱신때는 회원가입 로직이 
     실행되지 않도록 조건을 걸어둔 것!*/
-    userData.id !==undefined && 
-    axios.post('http://localhost:8888/user/join',{
+    userData.id !== undefined && 
+    axios.post('http://localhost:8888/user/signup',{
       userData : userData
     })
     .then((res)=>{
-      console.log('Success',res.data.result);
+      console.log(res.data.result);
       if (res.data.result == 'success'){
         alert('회원가입을 축하드립니다')
         nav('/')
@@ -52,6 +62,8 @@ function Join() {
         alert('중복입니다 다시입력하세요')
         idRef.current.value=''
         pwRef.current.value=''
+        nameRef.current.value=''
+        nickRef.current.value=''
         addRef.current.value=''
         idRef.current.focus()
       }
@@ -77,17 +89,42 @@ function Join() {
           <Form.Label>ID</Form.Label>
           <Form.Control type="text" placeholder="Enter id" ref={idRef}/>
         </Form.Group>
+      </Row>
 
+      <Row >
         <Form.Group as={Col} controlId="formGridPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" ref={pwRef}/>
         </Form.Group>
       </Row>
 
+      <Row >
+        <Form.Group as={Col} controlId="formGridCheckPassword">
+          <Form.Label>Check pw</Form.Label>
+          <Form.Control type="password" placeholder="Check Pw" ref={pwRef}/>
+        </Form.Group>
+      </Row>
+      
+      <Row >
+        <Form.Group as={Col} controlId="formGridName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Name" ref={nameRef}/>
+        </Form.Group>
+      </Row>
+
+      <Row >
+        <Form.Group as={Col} controlId="formGridNick">
+          <Form.Label>Nickname</Form.Label>
+          <Form.Control type="text" placeholder="Nickname" ref={nickRef}/>
+        </Form.Group>
+      </Row>
+
+      <Row>
       <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" ref={addRef}/>
+        <Form.Label>e-mail</Form.Label>
+        <Form.Control placeholder="" ref={addRef}/>
       </Form.Group>
+      </Row>
 
     
 
--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,32 @@ db_config.connect(conn)
 
 router.get('/',()=>{
     console.log('index Router');
-
+    // res.send(path.join(__dirname, "react-fairy/build/index.html"))
 })
 
+router.post('/user/signup', (req, res)=>{
+    console.log('signup Router', req.body);
+
+    //회원가입 시작
+    let sql = "insert into user_info values(?, ?, ?, ?, ?, 37.5, current_timestamp, 'U')"
+    // id, pw, name, nick, email
+    conn.query(sql
+        , [req.body.userData.id
+            , req.body.userData.pw
+            , req.body.userData.name
+            , req.body.userData.nick
+            , req.body.userData.add
+            ]
+        , (err, rows)=>{
+            if(rows) {
+                console.log('success signup')
+                res.json({result : 'success'})
+            } else {
+                console.log('faild to signup', err);
+            }
+    })
+    //회원가입 끝
+})
 
 
 
