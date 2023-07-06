64ac9f6b86ac4d8d942bb21276484052c5926e51
Holy <ktk0646@gmail.com>
2023-07-06 22:18:57 +0900

    ㅁㄹ

--- react-fairy/src/page/SignUp.jsx
+++ react-fairy/src/page/SignUp.jsx
@@ -8,10 +8,12 @@ import axios from 'axios'
 import {useNavigate} from 'react-router-dom'
 import Logo from '../Images/Logo.png'
 
-// html로 회원정보 관리하는거 가능 (nunjucks)
+// html로 회원정보 관리하는거 가능 (nunjucks) node Ex08DB
 // 아이디 중복체크 기능 ( 버튼식 또는 텍스트창 벗어나면 자동으로 체크 )
 //    >> DB에서 아이디를 PK? 로 등록해놔서 중복 알아서 걸러지긴 함..
 // 비밀번호 재확인 기능
+
+
 
 
 function Join() {
@@ -35,14 +37,16 @@ function Join() {
     ,nameRef.current.value
     ,nickRef.current.value
     ,addRef.current.value);
-        
+      
+
     // form이 submit 되지 못하도록 작업
     e.preventDefault();
 
 
     setUserData({
       id : idRef.current.value, 
-      pw : pwRef.current.value, 
+      pw : pwRef.current.value,
+      cpw : cpwRef.current.value,
       name : nameRef.current.value,
       nick : nickRef.current.value,
       add : addRef.current.value,      
@@ -55,19 +59,22 @@ function Join() {
     비어있는 값을 가지고 회원가입을 하면 안되니까 
     화면의 첫 갱신때는 회원가입 로직이 
     실행되지 않도록 조건을 걸어둔 것!*/
-    userData.id !== undefined && 
+    if(userData.id !== undefined && userData.pw === userData.cpw){
+      //  id 값이 초기상태인 undefined가 아니면서 pw, cpw가 일치할 때만 값을 전송함
     axios.post('http://localhost:8888/user/signup',{
       userData : userData
     })
+
     .then((res)=>{
       console.log(res.data.result);
       if (res.data.result == 'success'){
         alert('회원가입을 축하드립니다')
         nav('/')
       }else if(res.data.result == 'duplicated'){
-        alert('중복입니다 다시입력하세요')
+        alert('이미 등록된 아이디입니다. 다시 입력하세요') // 아이디 옆에 중복체크 버튼으로 다른 정보 입력 전에 아이디부터 확인해보기
         idRef.current.value=''
         pwRef.current.value=''
+        cpwRef.current.value=''
         nameRef.current.value=''
         nickRef.current.value=''
         addRef.current.value=''
@@ -78,12 +85,18 @@ function Join() {
     .catch(()=>{
       console.error('실패!')
     })
-
+  }else if(userData.id !== undefined && userData.pw !== userData.cpw){
+        // id가 초기상태가 아니면서 pw, cpw가 일치하지 않으면 비밀번호를 다시 입력하게
+    alert('비밀번호가 일치하지 않습니다')
+    pwRef.current.value=''
+    cpwRef.current.value=''
+    pwRef.current.focus()
+  }
   },[userData])
 
-  const id_check = ()=> {
-    alert('')
-  }
+  // const id_check = ()=> {
+  //   alert('')
+  // }
 
 
 
@@ -111,7 +124,7 @@ function Join() {
       <Row className='row1'>
         <Form.Group as={Col} controlId="formGridCheckPassword">
           <Form.Label>Check pw</Form.Label>
-          <Form.Control type="password" placeholder="Check Pw" ref={pwRef}/>
+          <Form.Control type="password" placeholder="Check Pw" ref={cpwRef}/>
         </Form.Group>
       </Row>
       
