7878a58ef61039f1d72e2dad2bda20220090bf9c
Holy <ktk0646@gmail.com>
2023-07-07 09:47:45 +0900

    console 오류 수정

--- react-fairy/src/page/Main.jsx
+++ react-fairy/src/page/Main.jsx
@@ -52,83 +52,83 @@ const Main = () => {
           <ul>
               <li>
                 <a href=""><img src='https://picsum.photos/id/250/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/251/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/252/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/253/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/254/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/255/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/256/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/257/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/258/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/259/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/260/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/261/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/270/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/263/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/264/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/265/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/266/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/267/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/268/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               <li>
                 <a href=""><img src='https://picsum.photos/id/269/150/150' alt="준비중.." /></a>
-                <a href="" data-name="VIEW MORE" class="btn"><h3>TIT</h3><p>CTT</p></a>
+                <a href="" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
               </li>
               
           </ul>
