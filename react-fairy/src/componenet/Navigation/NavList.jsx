8d85f28d9d5c764551fdc4a8674ff8829226913a
ShinePch <xkwhpch@gmail.com>
2023-07-10 22:16:14 +0900

    박충희_category

--- react-fairy/src/componenet/Navigation/NavList.jsx
+++ react-fairy/src/componenet/Navigation/NavList.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+
+import { useRef, useState } from 'react';
 
 
 const NavList = () => {
+  const hamElements = useRef(null);
+  const [showHamNav, setShowHamNav] = useState(false);
+  const handleClick = () => {
+    console.log('마우스 클릭 이벤트가 발생했습니다.');
+    setShowHamNav(!showHamNav);
+    // 추가 작업 수행
+  };
+
+
   return (
     <div className='Navi'>
-      <div className='ctg'>category
-        <ul className='ctgNav'>
+      <div className={`ham ${showHamNav ? 'active' : ''}`} ref={hamElements} onClick={handleClick}>ctg
+        <ul className={`hamNav ${showHamNav ? 'show' : ''}`}>
           <li><a href="">1</a></li>
           <li><a href="">2</a></li>
           <li><a href="">3</a></li>
