1456edddea33ff6c59c067d87fd9e8b1a7582b41
Holy <ktk0646@gmail.com>
2023-07-12 10:10:51 +0900

    ssdf

---
+++ react-fairy/src/page/TodayProd.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+
+const TodayProd = ({img1, barcode}) => {
+  return (
+    <div>
+<h2>오늘의 물품상품</h2>
+          <ul>
+              <li>
+                
+                <a href = {barcode} ><img src = {img1}/><p></p></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>{barcode}</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/251/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/252/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/253/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/254/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/255/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/256/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/257/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/258/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/259/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/260/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/261/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/270/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/263/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/264/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/265/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/266/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/267/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/268/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              <li>
+                <a href="#!"><img src='https://picsum.photos/id/269/150/150' alt="준비중.." /></a>
+                <a href="#!" data-name="VIEW MORE" className="btn"><h3>TIT</h3><p>CTT</p></a>
+              </li>
+              
+          </ul>
+
+          <button></button>
+    </div>
+  )
+}
+
+export default TodayProd
