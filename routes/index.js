06aa7f8867aa30bb32e184c65423948a749c33d5
Holy <ktk0646@gmail.com>
2023-07-13 17:34:16 +0900

    asd

--- routes/index.js
+++ routes/index.js
@@ -94,7 +94,7 @@ router.post('/user/login',(req, res)=>{
 
 // 카테고리 ------------------------- 시작
 
-router.post('/db/category', (req, res)=>{
+router.post('/db/categories', (req, res)=>{
     let sql = "select category_seq, prod_name, prod_photo, prod_price, prod_barcode from product_info"
    
     
