2323b4b945a51bba9d4150688ed967ec665f036a
ShinePch <xkwhpch@gmail.com>
2023-07-16 21:32:36 +0900

    대장김태경 외 박충희_분리중

--- react-fairy/src/js/PaymentComponet.jsx
+++ react-fairy/src/js/PaymentComponet.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import RealPayment from './RealPayment';
 
 const PaymentComponet = () => {
     useEffect(() => {
@@ -21,51 +22,53 @@ const PaymentComponet = () => {
         };
       }, []);
     
-      const RequestPay = () => {
-        const IMP = window.IMP;
-        IMP.request_pay(
-          {
-            pg: 'kcp',
-            pay_method: 'card',
-            merchant_uid: '57008833-33004',
-            name: '당근 10kg',
-            amount: 1004,
-            buyer_email: 'Iamport@chai.finance',
-            buyer_name: '포트원 기술지원팀',
-            buyer_tel: '010-1234-5678',
-            buyer_addr: '서울특별시 강남구 삼성동',
-            buyer_postcode: '123-456'
-          }, function(rsp) {
-            if (rsp.success) {   
-              // axios로 HTTP 요청
-              axios({
-                url: "3000/Detaill",
-                method: "post",
-                headers: { "Content-Type": "application/json" },
-                data: {
-                  imp_uid: rsp.imp_uid,
-                  merchant_uid: rsp.merchant_uid
-                }
-              }).then((data) => {
-                // 서버 결제 API 성공시 로직
-              })
-            } else {
-              alert(`결제에 실패하였습니다. 에러 내용: ${rsp.error_msg}`);
-            }
-          }
-        );
-      };
+      // const RealPayment = () => {
+      //   const IMP = window.IMP;
+      //   IMP.request_pay(
+      //     {
+      //       pg: 'kcp',
+      //       pay_method: 'card',
+      //       merchant_uid: '57008833-33004',
+      //       name: '당근 10kg',
+      //       amount: 1004,
+      //       buyer_email: 'Iamport@chai.finance',
+      //       buyer_name: '포트원 기술지원팀',
+      //       buyer_tel: '010-1234-5678',
+      //       buyer_addr: '서울특별시 강남구 삼성동',
+      //       buyer_postcode: '123-456'
+      //     }, function(rsp) {
+      //       if (rsp.success) {   
+      //         // axios로 HTTP 요청
+      //         axios({
+      //           url: "3000/Detaill",
+      //           method: "post",
+      //           headers: { "Content-Type": "application/json" },
+      //           data: {
+      //             imp_uid: rsp.imp_uid,
+      //             merchant_uid: rsp.merchant_uid
+      //           }
+      //         }).then((data) => {
+      //           // 서버 결제 API 성공시 로직
+      //         })
+      //       } else {
+      //         alert(`결제에 실패하였습니다. 에러 내용: ${rsp.error_msg}`);
+      //       }
+      //     }
+      //   );
+      // };
       
   return (
   <div>
     
     <ul class="DtButton">
-      <li><input type="button" class="cart" value="장바구니🖤" />
+      <li>
+        <input type="button" class="cart" value="장바구니🖤" />
       {/* <i class="fa-solid fa-heart"></i> */}
       </li>
 
       <li>
-        <input type="button" class="order" value="결제하기🛒" onClick={RequestPay}/>
+        <RealPayment/>
+        {/* <input type="button" class="order" value="결제하기🛒" onClick={RealPayment}/> */}
         {/* <i class="fa-solid fa-cart-shopping"></i> */}
       </li>
       
