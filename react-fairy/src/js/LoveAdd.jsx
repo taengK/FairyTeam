10180588ae984a727e2b64ede6c3e5fbd2c2700a
Holy <ktk0646@gmail.com>
2023-07-16 22:42:16 +0900

    흐아아앙압차

--- react-fairy/src/js/LoveAdd.jsx
+++ react-fairy/src/js/LoveAdd.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const LoveAdd = () => {
+const LoveAdd = ({prod_name, prod_price, prod_seq, prod_photo}) => {
+
+  const [userData, setUserData] = useState({})
+
+  const favorite = () =>{
+    sessionStorage.setItem('prod_name', prod_name)
+    sessionStorage.setItem('prod_price', prod_price)
+    sessionStorage.setItem('prod_seq', prod_seq)
+    sessionStorage.setItem('prod_photo', prod_photo)
+
+    if (sessionStorage.getItem('id') !== null) {
+      setUserData({
+        prod_name : {prod_name},
+        prod_price : {prod_price},
+        prod_seq : {prod_seq},
+        prod_photo : {prod_photo}
+      })
+     } else {
+      
+     }
+    
+    
+
+  }
+
+
   return (
     <div>
         <ul class="DtButton">
-      <li><input type="button" class="cart" value="장바구니🖤" />
+      <li><input type="button" class="cart" value="장바구니🖤" onClick={favorite}/>
       {/* <i class="fa-solid fa-heart"></i> */}
       </li>
         </ul>
