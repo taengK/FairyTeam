b7c27e76f989b09d5a25273f1ac9399086f1fa3d
Holy <ktk0646@gmail.com>
2023-07-16 09:56:13 +0900

    ㄴ

--- react-fairy/src/page/PostForm.jsx
+++ react-fairy/src/page/PostForm.jsx
@@ -268,7 +268,7 @@ function PostForm() {
                 </li>
                 <li>
                   <p>
-                    <input type="text" ref={imgUrlRef}/><button onClick={photoUpload}>업로드</button><br/>
+                    <input type="text" ref={imgUrlRef} placeholder='URL 주소를 입력하세요'/><button onClick={photoUpload}>업로드</button><br/>
                     <b>* 상품 이미지는 640x640에 최적화 되어 있습니다.</b><br />
                     - 상품 이미지는 PC에서는 1:1, 모바일에서는 1:1.23 비율로 보여집니다.<br />
                     - 이미지는 상품 등록 시 정사각형으로 잘려서 등록됩니다.<br />
