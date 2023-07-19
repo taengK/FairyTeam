2ff1c355e8bab67229df863f807948f53d53b5b1
taengK <ktk0646@gmail.com>
2023-07-19 19:03:47 +0900

    ㅎㅇ

--- react-fairy/src/componenet/Input/Input.js
+++ react-fairy/src/componenet/Input/Input.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Input.css';
 
 const openNewWindow = () => {
   window.open('http://localhost:3000/Kakao', '_blank', 'width=400,height=300');
 };
+const idRef = useRef()
+const resetChat =()=>{
+
+}
 
 const Input = ({ setMessage, sendMessage, message }) => (
  
-
+  
 
  <form className="form">
     
@@ -18,12 +22,13 @@ const Input = ({ setMessage, sendMessage, message }) => (
       type="text"
       placeholder="전송하려는 메시지를 입력하세요."
       value={message}
+      ref={idRef}
       //{ target: { value } }
       onChange={({ target: { value } })=> setMessage(value)}
       onKeyDown={(event) => event.key === 'Enter' ? sendMessage(event) : null}
     />
     
-    <button className="sendButton" onClick={(event) => sendMessage(event)}>전송</button>
+    <button className="sendButton" onClick={(event) => sendMessage(event)} >전송</button>
   </form>
 )
 
