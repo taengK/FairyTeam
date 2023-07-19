5ec71324abfd58223ed3a00c8c5f4b17930fb7a0
yeaeunii <dpdmsdl4543@gmail.com>
2023-07-19 20:47:41 +0900

    박충희 찐마지막 로고까지 마지막 진심 끝

--- react-fairy/src/componenet/Head/Head.jsx
+++ react-fairy/src/componenet/Head/Head.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Logo from '../../Images/Logo3.svg'
+import Logo from '../../Images/RealYourLog.svg'
 
 import { Link } from 'react-router-dom'
 
